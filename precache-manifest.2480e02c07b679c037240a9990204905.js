04d52ad5ac15f3f2916807e80f259cb41e83d1e2
unknown <zachary.romick@vanderbilt.edu>
2021-05-01 18:32:51 +0300

    Updates

---
+++ precache-manifest.2480e02c07b679c037240a9990204905.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2591f3542fe24b729ea98c076078443",
+    "url": "/URL/index.html"
+  },
+  {
+    "revision": "e5a26e8f56211a948528",
+    "url": "/URL/static/js/2.0938dda7.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/URL/static/js/2.0938dda7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4db3e279e874212f202",
+    "url": "/URL/static/js/main.9a937780.chunk.js"
+  },
+  {
+    "revision": "8c49bb604c6da91f6b22",
+    "url": "/URL/static/js/runtime-main.c65c050c.js"
+  }
+]);
