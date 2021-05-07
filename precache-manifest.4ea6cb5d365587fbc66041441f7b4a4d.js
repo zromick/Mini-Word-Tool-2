626d99d5e268d34fa3a901891063d587e8905696
unknown <zachary.romick@vanderbilt.edu>
2021-05-07 15:49:30 +0300

    Updates

---
+++ precache-manifest.4ea6cb5d365587fbc66041441f7b4a4d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d905f0c02aa6e360a7f6d44f956fa2ff",
+    "url": "/Mini-Word-Tool-2/index.html"
+  },
+  {
+    "revision": "cc002f57c73a7ce85a7f",
+    "url": "/Mini-Word-Tool-2/static/css/main.7fb8d89a.chunk.css"
+  },
+  {
+    "revision": "2e23606125419d77a4f3",
+    "url": "/Mini-Word-Tool-2/static/js/2.2acd4af6.chunk.js"
+  },
+  {
+    "revision": "856e22d3e3764738c36089f879e82a04",
+    "url": "/Mini-Word-Tool-2/static/js/2.2acd4af6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc002f57c73a7ce85a7f",
+    "url": "/Mini-Word-Tool-2/static/js/main.e073b00d.chunk.js"
+  },
+  {
+    "revision": "2a09b48bba8fcb3949a7",
+    "url": "/Mini-Word-Tool-2/static/js/runtime-main.dcecc76d.js"
+  }
+]);
