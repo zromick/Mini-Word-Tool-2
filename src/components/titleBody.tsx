dc893bdbbe8dca936bc0c14eaa0277410deddcfe
unknown <zachary.romick@vanderbilt.edu>
2021-09-04 12:16:37 -0500

    Renamed app to 'Replace Word Tool' and added an title input that allows users to replace the word 'Replace'

--- src/components/titleBody.tsx
+++ src/components/titleBody.tsx
@@ -1,20 +1,22 @@
 import React from 'react';
-import { Grid, Typography } from '@material-ui/core';
+import { Grid, Typography, Input } from '@material-ui/core';
 
 const TitleBody = () => {
   return (
     <Grid item container justify="center" xs={12}>
       <Grid item container justify="center" xs={12}>
-        <Typography variant="h3">Mini Word Tool</Typography>
+        <Typography variant="h3">
+          <Input
+            color="primary"
+            autoFocus
+            defaultValue="Replace"
+            inputProps={{ style: { fontSize: 45, width: 160 } }}
+          />
+          {` Word Tool`}
+        </Typography>
       </Grid>
       <Grid item container justify="center" xs={12}>
         <Typography>An Experimental Word Replacement Tool By <a href="https://github.com/zromick">Zac Romick</a></Typography>
-      </Grid>
-      <Grid item container justify="center" xs={12}>
-        <Typography><a href="https://github.com/zromick/Mini-Word-Tool-2/tree/master">Source Code and Documentation</a></Typography>
-      </Grid>
-      <Grid item container justify="center" xs={12}>
-        <Typography>Suggestions? Please send me an email: <a href="mailto:zachary.romick@vanderbilt.edu">zachary.romick@vanderbilt.edu</a></Typography>
       </Grid>
     </Grid>
   );
